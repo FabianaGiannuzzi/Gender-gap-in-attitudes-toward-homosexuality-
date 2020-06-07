6e3f13948dcddbbb1021ca18e074050c318319ff
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-06-07 13:05:16 +0200

    merged datasets 2-5

--- Analysis script.R
+++ Analysis script.R
@@ -1,14 +1,13 @@
 # Analysis script 
 # 1. Merging datasets 2, 4, 5 and 8 
+library(rio)
+rm(list = ls())
 
 
 ESS02rec <- import("D:/R Projects/Multivariate Analysis Project/ESS02 Recoded.dta")
 ESS04rec <- import("D:/R Projects/Multivariate Analysis Project/ESS04 Recoded.dta")
-
-
 ESS05rec <- import("D:/R Projects/Multivariate Analysis Project/ESS05 Recoded.dta")
 
 recoded_data <- rbind(ESS02rec, ESS04rec, ESS05rec)
-save(both_keywords_dataset, file = "both_keywords_fulldataset.RData")
 
-summary(ESS02rec)
+export(recoded_data, "ESS02-05 Recoded.dta")
